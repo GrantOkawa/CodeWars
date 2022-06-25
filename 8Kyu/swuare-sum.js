dd88a092b8898934e0832a0370b0f01a59784a1e
Grant <g.okawa@hotmail.com>
2022-06-25 17:43:46 -0400

    Added new codewars challenege

---
+++ 8Kyu/swuare-sum.js
@@ -0,0 +1,10 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+//My solution:
+function squareSum(numbers) {
+    let sum = 0
+    numbers.forEach( number => {
+        sum += (number * number)
+    })
+    return sum
+}
