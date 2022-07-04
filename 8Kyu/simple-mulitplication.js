092c136e04de9ccb9067f5cc7e77f05acef6ff56
Grant <g.okawa@hotmail.com>
2022-07-04 17:51:15 -0400

    Added new codewars challenege

---
+++ 8Kyu/simple-mulitplication.js
@@ -0,0 +1,6 @@
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+//My solution:
+function simpleMultiplication(number) {
+    return number * (number % 2 > 0 ? 9 : 8)
+}
